513339abab78ecc7467b6d77baaaed955adfdd38
氏弘一也 <ujihirokazuya@shihiroshikazuya-no-MacBook-Pro-2.local>
2017-10-29 20:46:34 +0900

    update

--- iris/train_test.py
+++ iris/train_test.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from data_set import DataSet
+import numpy as np
 from keras.models import load_model
 
 __author__ = "ujihirokazuya"
@@ -13,13 +14,25 @@ def train():
 
     data_set = DataSet(seq_length=seq_length, class_size_limit=class_size_limit, image_shape=image_shape)
     train_data_file_list, test_data_file_list = data_set.split_train_test()
-    X, y = data_set.get_all_sequences_in_memory(train_data_file_list)
-    X_test, y_test = data_set.get_all_sequences_in_memory(test_data_file_list)
+    # x, y = data_set.get_all_sequences_in_memory(train_data_file_list)
+    x_test, y_test = data_set.get_all_sequences_in_memory(test_data_file_list)
 
     # Get the model.
     model = load_model("result_tmp.h5")
-    loss_and_metrics = model.evaluate(X_test, y_test)
-    print("\nloss:{} accuracy:{}".format(loss_and_metrics[0],loss_and_metrics[1]))
+    # loss_and_metrics = model.evaluate(X_test, y_test)
+    # print("\nloss:{} accuracy:{}".format(loss_and_metrics[0],loss_and_metrics[1]))
+    # x_test = x_test[0]
+    x_test = x_test[9]
+    print(x_test.shape)
+    x_test = x_test[np.newaxis, :, :, :, :]
+    print(x_test.shape)
+    result = model.predict_classes(x_test, batch_size=1)
+    print(result)
+    result2 = model.predict(x_test, batch_size=1)
+    print(result2)
+    result3 = model.predict_proba(x_test, batch_size=1)
+    print(result3)
+    print(result3)
 
 
 def main():
